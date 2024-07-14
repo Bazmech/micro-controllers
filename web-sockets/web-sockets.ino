2ec5fa84f53d9e0e2eb51d7c1febb40fb07b053f
Barry <barry@bazmech.net>
2024-07-14 23:23:15 +0100

    feat: adding placeholder for websocket development

---
+++ web-sockets/web-sockets.ino
@@ -0,0 +1,17 @@
+/*
+
+https://stackoverflow.com/questions/69051106/c-or-c-websocket-client-working-example
+
+https://github.com/Links2004/arduinoWebSockets/tree/master/examples/esp32/WebSocketClient
+
+*/
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
