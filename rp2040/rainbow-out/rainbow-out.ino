69d05979190fccad22ccf256563180460b81f667
Barry <barry@bazmech.net>
2024-07-19 00:23:34 +0100

    feat: add rainbow out for future build

---
+++ rp2040/rainbow-out/rainbow-out.ino
@@ -0,0 +1,45 @@
+/*
+
+
+-----
+-----
+--x--    red
+-----
+-----
+
+-----
+-xxx-
+-xyx-    red/yellow
+-xxx-
+-----
+
+xxxxx
+xyyyx
+xyzyx    red/yellow/pink
+xyyyx
+xxxxx
+
+yyyyy
+yzzzy
+yzxzy    re/yellow/pink/green
+yzzzy
+yyyyy
+
+
+
+
+
+
+
+*/
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
